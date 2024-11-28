5351b3f9263009f080da78c325a2e35258a905b1
Saswata <saswata.24bcs10248@sst.scaler.com>
2024-11-28 22:11:28 +0530

    Changes made

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import logo from '../../logo.svg';
+import logo from '../../logo.png';
 import './navbar.css';
 
 const Navbar = () => {
@@ -14,10 +14,10 @@ const Navbar = () => {
         </div>
         <div className="gpt3__navbar-links_container">
           <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt3">What is GPT3?</a></p>
+          <p><a href="#wgpt3">What is Catalyst?</a></p>
           <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#features">Case Studies</a></p>
-          <p><a href="#blog">Library</a></p>
+          <p><a href="#features">Reviews</a></p>
+          {/* <p><a href="#blog">Library</a></p> */}
         </div>
       </div>
       <div className="gpt3__navbar-sign">
@@ -32,7 +32,7 @@ const Navbar = () => {
         <div className="gpt3__navbar-menu_container scale-up-center">
           <div className="gpt3__navbar-menu_container-links">
             <p><a href="#home">Home</a></p>
-            <p><a href="#wgpt3">What is GPT3?</a></p>
+            <p><a href="#wgpt3">What is Catalyst?</a></p>
             <p><a href="#possibility">Open AI</a></p>
             <p><a href="#features">Case Studies</a></p>
             <p><a href="#blog">Library</a></p>
