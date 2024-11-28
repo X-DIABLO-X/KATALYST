66b4fdf2fe64851cd9d3ce14dc1879532a59aa3d
VirajBh <virajbhanage00@gmail.com>
2024-11-28 23:03:08 +0530

    Footer

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -3,7 +3,7 @@ import './article.css';
 
 const Article = ({ imgUrl, date, text }) => (
   <div className="gpt3__blog-container_article">
-    <div className="gpt3__blog-container_article-image">
+    {/* <div className="gpt3__blog-container_article-image">
       <img src={imgUrl} alt="blog_image" />
     </div>
     <div className="gpt3__blog-container_article-content">
@@ -12,7 +12,7 @@ const Article = ({ imgUrl, date, text }) => (
         <h3>{text}</h3>
       </div>
       <p>Read Full Article</p>
-    </div>
+    </div> */}
   </div>
 );
 
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,21 +1,18 @@
-import React from 'react';
-import gpt3Logo from '../../logo.png';
-import './footer.css';
+import React from "react";
+import gpt3Logo from "../../logo.png";
+import "./footer.css";
 
 const Footer = () => (
   <div className="gpt3__footer section__padding">
     <div className="gpt3__footer-heading">
-      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+      
     </div>
 
-    <div className="gpt3__footer-btn">
-      <p>Request Early Access</p>
-    </div>
+    
 
     <div className="gpt3__footer-links">
       <div className="gpt3__footer-links_logo">
         <img src={gpt3Logo} alt="gpt3_logo" />
-        <p>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Links</h4>
@@ -32,14 +29,10 @@ const Footer = () => (
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Get in touch</h4>
-        <p>Crechterwoord K12 182 DK Alknjkcb</p>
-        <p>085-132567</p>
-        <p>info@payme.net</p>
+        <p>Electronic City, Bangalore Karnataka, India 560100</p>
+        <p>7044108465</p>
+        <p>catalyst24.forge@gmail.com</p>
       </div>
-    </div>
-
-    <div className="gpt3__footer-copyright">
-      <p>@2021 GPT-3. All rights reserved.</p>
     </div>
   </div>
 );
