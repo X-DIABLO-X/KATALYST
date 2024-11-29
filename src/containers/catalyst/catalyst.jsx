347449b39e79974fe47c3305dba67b04eafaa933
Saswata <saswata.24bcs10248@sst.scaler.com>
2024-11-29 15:39:52 +0530

    Changes in What is Catalyst

--- src/containers/catalyst/catalyst.jsx
+++ src/containers/catalyst/catalyst.jsx
@@ -5,7 +5,11 @@ import './catalyst.css';
 const catalyst = () => (
   <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
     <div className="gpt3__whatgpt3-feature">
-      <Feature title="What is Catalyst?" text="Catalyst is an AI-powered creativity amplifier that transforms raw ideas into groundbreaking, actionable concepts. By merging your thoughts with global insights, it fosters originality, collaboration, and innovation - unlocking the potential to change the game" />
+      <Feature title="What is Catalyst?" text="What sets your ideas apart? What if your AI could understand your unique goals, budget, and market challenges as if it were built just for you? 
+       Here’s what makes Catalyst truly stand out:
+       Catalyst is an AI-powered creativity amplifier that transforms raw ideas into groundbreaking, actionable concepts. 
+       In a world flooded with generic solutions, Catalyst dares to ask: Why settle for ordinary when extraordinary is within reach?
+       By merging your thoughts with global insights, it fosters originality, collaboration, and innovation - unlocking the potential to change the game" />
     </div>
     <div className="gpt3__whatgpt3-heading">
       <h1 className="gradient__text">Your Partner in navigating life </h1>
