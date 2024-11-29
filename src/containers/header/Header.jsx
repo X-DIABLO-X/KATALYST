afc2cb338f29d70f87b5ac6632b4d87a1661e0ad
Saswata <saswata.24bcs10248@sst.scaler.com>
2024-11-29 15:22:14 +0530

    header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,8 +6,8 @@ import './header.css';
 const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
-      <h1 className="gradient__text">Let&apos;s Build Something amazing with Catalyst</h1>
-      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      <h1 className="gradient__text">Let's Ignite Your Imagination with Catalyst</h1>
+      <p>Transform your raw ideas into personalized, actionable strategies tailored to your goals. Say goodbye to generic solutions and embrace a future where every vision is amplified, every challenge conquered, and every possibility reimagined</p>
 
       <div className="gpt3__header-content__input">
         {/* <input type="email" placeholder="Your Email Address" /> */}
